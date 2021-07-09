0b9fcc6fbae63b15370d00716a401f6048b1e87f
Brett Treweek <bretttrew@gmail.com>
2021-07-09 20:03:20 +0800

    started schema and server.js

---
+++ models/Workout.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const workoutSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    }
+})
+const Workout = mongoose.model('Workout', workoutSchema)
+module.exports = Workout
